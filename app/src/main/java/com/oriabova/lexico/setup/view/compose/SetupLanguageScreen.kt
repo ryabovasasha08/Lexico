b5a4df6fcb542fb7ccdfc95f5c10895c4b35c38a
ryabovasasha08 <oleksandra.riabova@parshipgroup.com>
2025-05-07 17:27:11 +0200

    Improve SelectLanguage

--- app/src/main/java/com/oriabova/lexico/setup/view/compose/SetupLanguageScreen.kt
+++ app/src/main/java/com/oriabova/lexico/setup/view/compose/SetupLanguageScreen.kt
@@ -1,23 +1,28 @@
 package com.oriabova.lexico.setup.view.compose
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,40 +38,39 @@ import com.oriabova.app.R
 import com.oriabova.lexico.root.view.theme.Colors
 import com.oriabova.lexico.root.view.theme.LexicoFont
 import com.oriabova.lexico.root.view.theme.LexicoTheme
+import kotlinx.coroutines.delay
 import java.util.Locale
 
 private val TopPadding = 30.dp
-private val ScreenVerticalSpacing = 16.dp
 private val CardCornerRadius = 15.dp
 private val BorderStrokeWidth = 1.dp
 private val CardInnerPaddingHorizontal = 16.dp
 private val CardInnerPaddingVertical = 8.dp
 private val ButtonVerticalPadding = 36.dp
 private val LanguageItemPadding = 8.dp
-private val SelectedLanguageIconPadding = 16.dp
+private const val SelectedItemAnimationDuration = 300L
 
 @Composable
 fun SetupLanguageScreen(onSetupLanguageComplete: (String?) -> Unit) {
-    var selectedLanguage by remember { mutableStateOf<String?>(null) }
+    var mask: String by remember { mutableStateOf("") }
 
     SetupScreenWrapper {
         Column(
             modifier = Modifier.padding(top = TopPadding, bottom = ButtonVerticalPadding),
-            verticalArrangement = Arrangement.spacedBy(ScreenVerticalSpacing),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Title()
+            Spacer(modifier = Modifier.height(4.dp))
             Subtitle()
+            Spacer(modifier = Modifier.height(16.dp))
+            SearchField(mask) { mask = it }
+            Spacer(modifier = Modifier.height(16.dp))
             LanguagePicker(
-                selectedLanguage = selectedLanguage,
+                mask = mask,
                 modifier = Modifier.weight(1f),
-                onLanguagePicked = { selectedLanguage = it }
+                onLanguagePicked = { onSetupLanguageComplete(it) }
             )
-
-            CtaButton(
-                isEnabled = selectedLanguage.isNullOrBlank().not(),
-                onClick = { onSetupLanguageComplete(selectedLanguage) }
-            )
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
@@ -75,8 +80,8 @@ private fun Title() {
     Text(
         text = stringResource(id = R.string.setup_language_title),
         modifier = Modifier.fillMaxWidth(),
-        style = LexicoFont.d100(color = Colors.primary030),
-        textAlign = TextAlign.Left
+        style = LexicoFont.b200Default(color = Colors.primary020),
+        textAlign = TextAlign.Center
     )
 }
 
@@ -85,23 +90,50 @@ private fun Subtitle() {
     Text(
         text = stringResource(id = R.string.setup_language_subtitle),
         modifier = Modifier.fillMaxWidth(),
-        style = LexicoFont.f100Default(color = Colors.primary030),
-        textAlign = TextAlign.Left
+        style = LexicoFont.f100Default(color = Colors.primary020.copy(alpha = 0.7f)),
+        textAlign = TextAlign.Center
     )
 }
 
 @Composable
-private fun CtaButton(isEnabled: Boolean, onClick: () -> Unit) {
-    TextButton(
-        text = stringResource(id = R.string.setup_language_cta_text),
-        isEnabled = isEnabled,
-        onClick = onClick,
-    )
+private fun SearchField(text: String, onTextChange: (String) -> Unit) {
+    DecorationBox(textAlignment = Alignment.CenterVertically) {
+        TextField(
+            value = text,
+            onValueChange = onTextChange,
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text(stringResource(R.string.setup_language_input_hint)) },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Colors.supportLight,
+                unfocusedContainerColor = Colors.supportLight,
+            ),
+            shape = RoundedCornerShape(CardCornerRadius),
+            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences)
+        )
+    }
+}
+
+@Composable
+private fun DecorationBox(
+    textAlignment: Alignment.Vertical,
+    textField: @Composable () -> Unit,
+) {
+    Row {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .padding(vertical = 12.dp)
+                .align(textAlignment),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            textField()
+        }
+    }
 }
 
 @Composable
 private fun LanguagePicker(
-    selectedLanguage: String?,
+    mask: String,
     modifier: Modifier = Modifier,
     onLanguagePicked: (String) -> Unit
 ) {
@@ -109,6 +141,7 @@ private fun LanguagePicker(
         .map { it.displayLanguage }
         .distinct()
         .filterNot { it.isBlank() }
+        .filter { it.contains(mask, ignoreCase = true) }
         .sorted()
 
     val languagePickerState = rememberLazyListState()
@@ -123,11 +156,9 @@ private fun LanguagePicker(
             val itemModifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = LanguageItemPadding)
-            val isSelected = selectedLanguage == language
 
             LanguageItem(
                 language,
-                isSelected,
                 itemModifier
             ) { onLanguagePicked(language) }
         }
@@ -137,17 +168,31 @@ private fun LanguagePicker(
 @Composable
 private fun LanguageItem(
     language: String,
-    isSelected: Boolean,
     modifier: Modifier = Modifier,
-    onClick: () -> Unit
+    onSelected: () -> Unit
 ) {
+    var isSelected by remember { mutableStateOf(false) }
+
+    LaunchedEffect(isSelected) {
+        if (isSelected) {
+            delay(SelectedItemAnimationDuration)
+            onSelected()
+        }
+    }
+
+    val backgroundColor by animateColorAsState( // Use animateColorAsState
+        targetValue = if (isSelected) Colors.primary030 else Colors.primary200, // Slightly darker
+        animationSpec = tween(durationMillis = 200), // Short animation
+        label = "background color"
+    )
+
+    val textColor = if (isSelected) Colors.support700 else Colors.support500
+
     Card(
-        onClick = onClick,
+        onClick = { isSelected = true },
         colors = CardDefaults.cardColors(
-            containerColor = Colors.primary030,
-            disabledContainerColor = Colors.primary030,
+            containerColor = backgroundColor,
             contentColor = Colors.support500,
-            disabledContentColor = Colors.support500,
         ),
         shape = RoundedCornerShape(CardCornerRadius),
         border = BorderStroke(BorderStrokeWidth, Colors.primary500),
@@ -162,20 +207,9 @@ private fun LanguageItem(
                         vertical = CardInnerPaddingVertical
                     ),
                 text = language,
-                style = LexicoFont.f100Default(color = Colors.primary500),
+                style = LexicoFont.f100Default(textColor),
                 textAlign = TextAlign.Center,
             )
-
-            if (isSelected) {
-                Icon(
-                    imageVector = Icons.Default.Check,
-                    contentDescription = null,
-                    tint = Colors.primary500,
-                    modifier = Modifier
-                        .align(Alignment.CenterEnd)
-                        .padding(end = SelectedLanguageIconPadding)
-                )
-            }
         }
 
     }
--- app/src/main/java/com/oriabova/lexico/setup/view/compose/SetupScreenWrapper.kt
+++ app/src/main/java/com/oriabova/lexico/setup/view/compose/SetupScreenWrapper.kt
@@ -2,6 +2,7 @@ package com.oriabova.lexico.setup.view.compose
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -10,18 +11,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.oriabova.lexico.root.view.theme.Colors
 
-private val HorizontalScreenPadding = 16.dp
+private val ScreenPadding = 16.dp
 
 @Composable
 internal fun SetupScreenWrapper(content: @Composable BoxScope.() -> Unit) {
     Scaffold(
         containerColor = Colors.primary500,
+        contentWindowInsets = WindowInsets(0),
         content = { paddingValues ->
             Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(paddingValues)
-                    .padding(horizontal = HorizontalScreenPadding)
+                    .padding(ScreenPadding)
             ) {
                 content()
             }
