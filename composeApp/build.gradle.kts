5eb49c51b3377b82821489397c734469ebc00b77
ryabovasasha08 <ryabovasasha008@gmail.com>
2025-12-20 22:36:19 +0100

    Fix Serialization

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.gradle.kotlin.dsl.implementation
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -6,6 +5,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
--- composeApp/src/commonMain/kotlin/com/oriabova/lexico/serialization/KotlinXSerializer.kt
+++ composeApp/src/commonMain/kotlin/com/oriabova/lexico/serialization/KotlinXSerializer.kt
@@ -1,12 +1,13 @@
 package com.oriabova.lexico.serialization
 
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.KSerializer
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.serializer
 import kotlin.reflect.KClass
 
 private const val NULL_CONTENT = "null"
 
+@OptIn(InternalSerializationApi::class)
 class KotlinXSerializer(
     private val json: Json,
 ) : JsonSerializer {
@@ -19,12 +20,5 @@ class KotlinXSerializer(
 
     override fun <T : Any> toJson(data: T, cls: KClass<T>): String {
         return json.encodeToString(cls.serializer(), data)
-    }
-
-    @OptIn(ExperimentalSerializationApi::class)
-    @Suppress("UNCHECKED_CAST")
-    private fun <T : Any> KClass<T>.serializer(): KSerializer<T> {
-        val kSerializer = json.serializersModule.getContextual(this) as KSerializer<T>
-        return kSerializer
     }
 }
