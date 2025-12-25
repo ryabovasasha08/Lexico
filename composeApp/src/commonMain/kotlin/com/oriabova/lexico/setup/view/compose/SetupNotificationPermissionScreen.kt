31aaafc48bfe958547f6fe900f28d4bee322bbc7
ryabovasasha08 <ryabovasasha008@gmail.com>
2025-12-25 14:13:38 +0100

    Fix screen updating on "don't allow" click

--- composeApp/src/commonMain/kotlin/com/oriabova/lexico/setup/view/compose/SetupNotificationPermissionScreen.kt
+++ composeApp/src/commonMain/kotlin/com/oriabova/lexico/setup/view/compose/SetupNotificationPermissionScreen.kt
@@ -43,7 +43,7 @@ fun SetupNotificationPermissionScreen(onPermissionGranted: () -> Unit) {
 
     CheckPermissionOnResume(
         onPermissionGranted = onPermissionGranted,
-        onPermissionDenied = { permissionDenied = false }
+        onPermissionDenied = { permissionDenied = true }
     )
 
     SetupScreenWrapper {
@@ -73,7 +73,7 @@ fun SetupNotificationPermissionScreen(onPermissionGranted: () -> Unit) {
             } else {
                 InitialFooter(
                     onPermissionGranted = onPermissionGranted,
-                    onPermissionDenied = { permissionDenied = false }
+                    onPermissionDenied = { permissionDenied = true }
                 )
             }
         }
